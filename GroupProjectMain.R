d8beaa47b327f7b589a989d11942543e3b6fb89f
c3kimball <c3kimball@student.bridgew.edu>
2022-12-05 14:54:09 -0500

    Importing dataset

--- GroupProjectMain.R
+++ GroupProjectMain.R
@@ -1,2 +1,5 @@
-# Response variable = gas prices by month
+# Craig Kimball 12/5/2022
 
+gasPrices <- MonthlyGasPricesInMassachusetts$`Massachusetts All Grades All Formulations Retail Gasoline Prices Dollars per Gallon`
+crudeOilPrices <- MonthlyGasPricesInMassachusetts$`Cushing OK WTI Spot Price FOB Dollars per Barrel`
+massMinWage <- MonthlyGasPricesInMassachusetts$`Massachusetts Minimum Wage in Dollars`
