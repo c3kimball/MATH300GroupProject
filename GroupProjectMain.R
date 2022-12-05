c091a2f4a9299755059b020a6bfba1094615727f
c3kimball <c3kimball@student.bridgew.edu>
2022-12-05 15:11:56 -0500

    Calculating a confidence interval for monthly gas prices

--- GroupProjectMain.R
+++ GroupProjectMain.R
@@ -1,5 +1,14 @@
 # Craig Kimball 12/5/2022
 
+
 gasPrices <- MonthlyGasPricesInMassachusetts$`Massachusetts All Grades All Formulations Retail Gasoline Prices Dollars per Gallon`
 crudeOilPrices <- MonthlyGasPricesInMassachusetts$`Cushing OK WTI Spot Price FOB Dollars per Barrel`
 massMinWage <- MonthlyGasPricesInMassachusetts$`Massachusetts Minimum Wage in Dollars`
+
+# Confidence Interval of Gas Price
+gasPriceCritValue = qt(.975, length(gasPrices - 1))
+gasPricesStdErr = sd(gasPrices)/sqrt(length(gasPrices))
+marginErrorGasPrices = gasPriceCritValue * gasPricesStdErr
+
+lowerBoundGasPrice = mean(gasPrices) - marginErrorGasPrices
+upperBoundGasPrice = mean(gasPrices) + marginErrorGasPrices
