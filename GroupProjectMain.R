b12d3c1dc4676dd651a3e78508c54a804426f053
c3kimball <c3kimball@student.bridgew.edu>
2022-12-12 20:50:43 -0500

    Creating graphs and more data

--- GroupProjectMain.R
+++ GroupProjectMain.R
@@ -4,9 +4,9 @@
 # Juliana Patrone
 
 
-gasPrices <- MonthlyGasPricesInMassachusetts$`Massachusetts All Grades All Formulations Retail Gasoline Prices Dollars per Gallon`
-crudeOilPrices <- MonthlyGasPricesInMassachusetts$`Cushing OK WTI Spot Price FOB Dollars per Barrel`
-massMinWage <- MonthlyGasPricesInMassachusetts$`Massachusetts Minimum Wage in Dollars`
+gasPrices <- MonthlyGasPricesInMassachusetts$`Gas Prices`
+crudeOilPrices <- MonthlyGasPricesInMassachusetts$`Crude Oil Prices`
+massMinWage <- MonthlyGasPricesInMassachusetts$`Massachusetts Minimum Wage`
 
 # Confidence Interval of Gas Price
 gasPriceCritValue = qt(.975, length(gasPrices - 1))
@@ -16,7 +16,29 @@ marginErrorGasPrices = gasPriceCritValue * gasPricesStdErr
 lowerBoundGasPrice = mean(gasPrices) - marginErrorGasPrices
 upperBoundGasPrice = mean(gasPrices) + marginErrorGasPrices
 
+# Unpooled t-test of Gas Prices and Crude Oil Prices
+t.test(crudeOilPrices, gasPrices, var.equal = FALSE)
+
+# Unpooled t-test of Gas Prices and Minimum Wage
+t.test(massMinWage, gasPrices, var.equal = FALSE)
+
+# While running the unpooled t-tests we noticed that the p-values are the same
+# This could be expected from the data, or being caused from user error
+
 # Regression Model of Gas Prices
 model <- lm(gasPrices~crudeOilPrices + massMinWage)
 pairs(~., data = MonthlyGasPricesInMassachusetts)
+pairs(~., data = MonthlyGasPricesInMassachusetts[ ,c(-1, -1)])
+plot(crudeOilPrices, gasPrices) # We want to fit a line of best fit to this graph, but don't know how
+plot(massMinWage, gasPrices)
+
+summary(model)
+
+# Examining the residuals
+qqnorm(model$residuals)
+qqline(model$residuals)
+hist(model$residuals)
+plot(model)
+cor(MonthlyGasPricesInMassachusetts[ , c(0, -1)])
+# Ask about the correct syntax of this function
 
